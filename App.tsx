560a458fe98a3f68450dd3cadfbf0e37d79df540
Douglas Lowder <douglowder@mac.com>
2024-04-03 19:30:33 -0700

    Expo canary, RNTV 0.74, Android Touchable patch

--- App.tsx
+++ App.tsx
@@ -1,12 +1,150 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  useTVEventHandler,
+  Platform,
+  Pressable,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [remoteEventLog, setRemoteEventLog] = useState<string[]>([]);
+  const [pressableEventLog, setPressableEventLog] = useState<string[]>([]);
+
+  const logWithAppendedEntry = (log: string[], entry: string) => {
+    const limit = 6;
+    const newEventLog = log.slice(0, limit - 1);
+    newEventLog.unshift(entry);
+    return newEventLog;
+  };
+
+  const updatePressableLog = (entry: string) => {
+    setPressableEventLog((log) => logWithAppendedEntry(log, entry));
+  };
+
+  useTVEventHandler((event) => {
+    const { eventType, eventKeyAction } = event;
+    if (eventType !== 'focus' && eventType !== 'blur') {
+      setRemoteEventLog((log) =>
+        logWithAppendedEntry(
+          log,
+          `type=${eventType}, action=${
+            eventKeyAction !== undefined ? eventKeyAction : ''
+          }`,
+        ),
+      );
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <PressableButton title="Pressable" log={updatePressableLog} />
+      <TouchableOpacityButton
+        title="TouchableOpacity"
+        log={updatePressableLog}
+      />
+      <TouchableHighlightButton
+        title="TouchableHighlight"
+        log={updatePressableLog}
+      />
+      {Platform.OS === 'android' ? (
+        <TouchableNativeFeedbackButton
+          title="TouchableNativeFeedback"
+          log={updatePressableLog}
+        />
+      ) : null}
+
+      <View style={styles.logContainer}>
+        <Text style={styles.logText}>{remoteEventLog.join('\n')}</Text>
+        <Text style={styles.logText}>{pressableEventLog.join('\n')}</Text>
+      </View>
     </View>
   );
 }
+
+const PressableButton = (props: {
+  title: string;
+  log: (entry: string) => void;
+}) => {
+  return (
+    <Pressable
+      onFocus={() => props.log(`${props.title} focus`)}
+      onBlur={() => props.log(`${props.title} blur`)}
+      onPress={() => props.log(`${props.title} pressed`)}
+      onLongPress={() => props.log(`${props.title} long press`)}
+      style={({ pressed, focused }) =>
+        pressed || focused ? styles.pressableFocused : styles.pressable
+      }
+    >
+      {({ focused }) => {
+        return (
+          <Text style={styles.pressableText}>
+            {focused ? `${props.title} focused` : props.title}
+          </Text>
+        );
+      }}
+    </Pressable>
+  );
+};
+
+const TouchableOpacityButton = (props: {
+  title: string;
+  log: (entry: string) => void;
+}) => {
+  return (
+    <TouchableOpacity
+      style={styles.pressable}
+      onFocus={() => props.log(`${props.title} focus`)}
+      onBlur={() => props.log(`${props.title} blur`)}
+      onPress={() => props.log(`${props.title} pressed`)}
+      onLongPress={() => props.log(`${props.title} long press`)}
+    >
+      <Text style={styles.pressableText}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const TouchableHighlightButton = (props: {
+  title: string;
+  log: (entry: string) => void;
+}) => {
+  return (
+    <TouchableHighlight
+      style={styles.pressable}
+      onFocus={() => props.log(`${props.title} focus`)}
+      onBlur={() => props.log(`${props.title} blur`)}
+      onPress={() => props.log(`${props.title} pressed`)}
+      onLongPress={() => props.log(`${props.title} long press`)}
+    >
+      <Text style={styles.pressableText}>{props.title}</Text>
+    </TouchableHighlight>
+  );
+};
+
+const TouchableNativeFeedbackButton = (props: {
+  title: string;
+  log: (entry: string) => void;
+}) => {
+  return (
+    <TouchableNativeFeedback
+      background={TouchableNativeFeedback.SelectableBackground()}
+      onFocus={() => props.log(`${props.title} focus`)}
+      onBlur={() => props.log(`${props.title} blur`)}
+      onPress={() => props.log(`${props.title} pressed`)}
+      onLongPress={() => props.log(`${props.title} long press`)}
+    >
+      <View style={styles.pressable}>
+        <Text style={styles.pressableText}>{props.title}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+};
+
+const scale = Platform.isTV && Platform.OS === 'ios' ? 2 : 1;
 
 const styles = StyleSheet.create({
   container: {
@@ -14,5 +152,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logContainer: {
+    flexDirection: 'row',
+    padding: 5 * scale,
+    margin: 5 * scale,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  logText: {
+    height: 100 * scale,
+    width: 150 * scale,
+    fontSize: 10 * scale,
+    margin: 5 * scale,
+    alignSelf: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  pressable: {
+    borderColor: 'blue',
+    backgroundColor: 'blue',
+    borderWidth: 1,
+    borderRadius: 5 * scale,
+    margin: 5 * scale,
+  },
+  pressableFocused: {
+    borderColor: 'blue',
+    backgroundColor: '#000088',
+    borderWidth: 1,
+    borderRadius: 5 * scale,
+    margin: 5 * scale,
+  },
+  pressableText: {
+    color: 'white',
+    fontSize: 15 * scale,
   },
 });
